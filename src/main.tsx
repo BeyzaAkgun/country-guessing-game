87783eb10d8e298f763ffe76556a756587db0ee0
BeyzaAkgun <beyzaakgun@hotmail.com>
2026-09-17 22:20:42 +0300

    Remove commented-out code and clean up comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "@/app/App";
-// import "@/styles/index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-
 //main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
